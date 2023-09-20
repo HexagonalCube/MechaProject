1f3509946cb707c25d097494194801ee058b6c00
HexagonalCube <83779758+HexagonalCube@users.noreply.github.com>
2023-09-20 20:26:51 -0300

    Update PlayerFov.cs

--- Assets/Scripts/PlayerFov.cs
+++ Assets/Scripts/PlayerFov.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Rotates the FOV game object. Needs controller to call commands.
+/// <summary>
+/// Rotates the FOV game object. Needs controller to call commands.
+/// </summary>
 public class PlayerFov : MonoBehaviour
 {
     [SerializeField] GameObject fov;
     public void TurnLeft()
     {
-        fov.transform.rotation = Quaternion.Euler(90, 0, 0);
+        fov.transform.rotation = Quaternion.Euler(0, 0, 90);
+        Debug.Log("LookingLEFT");
     }
     public void TurnRight()
     {
-        fov.transform.rotation = Quaternion.Euler(-90, 0, 0);
+        fov.transform.rotation = Quaternion.Euler(0, 0, -90);
+        Debug.Log("LookingRIGHT");
     }
     public void TurnUp()
     {
         fov.transform.rotation = Quaternion.Euler(0, 0, 0);
+        Debug.Log("LookingUP");
     }
     public void TurnDown()
     {
-        fov.transform.rotation = Quaternion.Euler(180, 0, 0);
+        fov.transform.rotation = Quaternion.Euler(0, 0, 180);
+        Debug.Log("LookingDOWN");
     }
 }
